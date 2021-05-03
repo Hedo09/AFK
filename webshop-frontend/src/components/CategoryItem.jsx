a86c3cc1eb23d346721a605f4e7db54471e6f1b9
Hedo09 <hedrichadam09@gmail.com>
2021-05-03 20:50:03 +0200

    init

--- webshop-frontend/src/components/CategoryItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CategoryItem = (props) => (
-    <li key={props.id} data-id={props.id} onClick={props.onClick}><a>{props.name}</a></li>
-);
-
-export default CategoryItem;
--- webshop-frontend/src/components/RowItemUser.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const RowItemUser = (props) => (
-    <tr>{props.name}</tr>
-);
-
-export default RowItemUser;
